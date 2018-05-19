87e350b3408e3a66e6527241fc473a9d57e22309
apasny <apasov157@gmail.com>
2018-05-19 03:18:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+  app.listen(PORT, () => console.log(`Listening port ${PORT}`));
