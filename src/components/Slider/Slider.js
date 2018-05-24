b271fc365603b6e8e184c9375730bd43bb9a2c57
apasny <apasov157@gmail.com>
2018-05-25 01:10:49 +0300

    new

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './slider.css';
 
 export default () => {
   return (
-    <div>
+    <div className="slider_section">
       Slider
+      <div className="dashed_line top"></div>
+      <div className="dashed_line bottom"></div>
     </div>
   )
 }
